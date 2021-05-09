aec1c547315ea3f68403bb0e3b7b459dff23aaf8
vase444ka <vborysyuk@live.com>
2021-05-09 18:45:00 +0300

    lab5 implemented with animation

--- lab5/main.py
+++ lab5/main.py
@@ -1,28 +1,43 @@
+import time
+
 import matplotlib.pyplot as plt
-import numpy as np
+from celluloid import Camera
 from sympy import *
 from matplotlib.collections import LineCollection
 import random
-
-from sympy.vector import Vector
 
 MINX = 0.
 MAXX = 10.
 MINY = 0.
 MAXY = 10.
 
-# number of slabs
-k = 10
-
 # total number of points
 n = 50
 
+def draw():
+    ax.set_xlim([MINX - 1, MAXX + 1])
+    ax.set_ylim([MINY - 1, MAXY + 1])
+    plt.scatter(list(map(lambda x: x[0], points)), list(map(lambda x: x[1], points)), c=colors)
 
-def generate_points(n):
-    points = []
+
+def draw_step(segment, farmost_point):
+    draw()
+    plt.plot(list(map(lambda x: x[0], segment)), list(map(lambda x: x[1], segment)), 'go-')
+    plt.plot(farmost_point[0], farmost_point[1], 'go-')
+    camera.snap()
+
+
+def define_hull(hull):
+    draw()
+    hull.append(hull[0])
+    plt.plot(list(map(lambda x: x[0], hull)), list(map(lambda x: x[1], hull)), 'bo-')
+    camera.snap()
+
+def generate_points():
+    generated_points = []
     for i in range(n):
-        points.append([random.uniform(MINX, MAXX), random.uniform(MINY, MAXY)])
-    return points
+        generated_points.append([random.uniform(MINX, MAXX), random.uniform(MINY, MAXY)])
+    return generated_points
 
 
 def distance_to_segment(point, segment):
@@ -45,6 +60,9 @@ def rec_fh(current_points, segment):
         return []
     farmost_point = max(current_points,
                         key=lambda cur: distance_to_segment(cur, segment))
+    colors[points.index(farmost_point)] = "b"
+
+    draw_step(segment, farmost_point)
     left_points = []
     right_points = []
     for point in points:
@@ -59,7 +77,7 @@ def rec_fh(current_points, segment):
            rec_fh(right_points, (farmost_point, segment[1]))
 
 
-def fast_hull(points):#TODO order in return
+def fast_hull(points):
     leftmost = min(points)
     rightmost = max(points)
     left_points = []
@@ -71,32 +89,20 @@ def fast_hull(points):#TODO order in return
             if is_left_turn(rightmost, leftmost, point):
                 right_points.append(point)
 
-    print(leftmost)
-    print(rightmost)
-    print(left_points)
-    print(right_points)
 
     return [leftmost] + rec_fh(left_points, (leftmost, rightmost)) + \
            [rightmost] + rec_fh(right_points, (rightmost, leftmost))
 
 
-def draw(points=[], hull=[]):
-    fig, ax = plt.subplots()
-    ax.set_xlim([MINX - 1, MAXX + 1])
-    ax.set_ylim([MINY - 1, MAXY + 1])
-
-    plt.plot(list(map(lambda x: x[0], points)), list(map(lambda x: x[1], points)), 'r.')
-    print(hull)
-    plt.plot(list(map(lambda x: x[0], hull)), list(map(lambda x: x[1], hull)), 'bo')
-
-    lines = [[hull[-1], hull[0]]]
-    lc = LineCollection(lines, linewidths=1)
-    ax.add_collection(lc)
-
-    plt.show()
-
-
 if __name__ == '__main__':
-    points = generate_points(n)
+    points = generate_points()
+    colors = ["r"] * len(points)
+    fig, ax = plt.subplots()
+    camera = Camera(fig)
+
     hull = fast_hull(points)
-    draw(points, hull)
+
+    define_hull(hull)
+
+    anim = camera.animate(repeat=False, interval=3000)
+    plt.show()
