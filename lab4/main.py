bbe9a926bb16f9bb8fef155feaa50a837e59ace1
vase444ka <vborysyuk@live.com>
2021-05-14 14:33:31 +0300

    lab4 implemented and animated

--- lab4/main.py
+++ lab4/main.py
@@ -1,3 +1,5 @@
+import math
+
 import matplotlib.pyplot as plt
 from celluloid import Camera
 import random
@@ -12,29 +14,35 @@ MAXY = 10.
 n = 15
 
 # number of points for jarvis method
-n_test = 7
+n_test = 15
 
 
 class Node:
-    def __init__(self, point, points_list, left_node = None, right_node = None):
+    def __init__(self, point, points_list, minx, maxx, miny, maxy, left_node=None, right_node=None):
         self.point = point
+        self.miny = miny
+        self.maxy = maxy
+        self.minx = minx
+        self.maxx = maxx
         self.points_list = points_list
         self.left_node = left_node
         self.right_node = right_node
 
 
-def draw_tree(node, is_vertical_split=True, color='y'):
+def draw_tree(node, is_vertical_split=True, color=(1., 0., 0.)):
     if is_vertical_split:
         plt.plot([node.point[0], node.point[0]],
-                 [MINY - 1, MAXY + 1],color)
+                 [node.miny, node.maxy], color=color)
     else:
-        plt.plot([MINX - 1, MAXX + 1],
-                 [node.point[1], node.point[1]],
-                 color)
-    if node.left_node != None:
-        draw_tree(node.left_node, not is_vertical_split)
-    if node.right_node != None:
-        draw_tree(node.right_node, not is_vertical_split)
+        plt.plot([node.minx, node.maxx],
+                 [node.point[1], node.point[1]], color=color)
+
+    increment = 1/(round(math.log2(n)) + 3)
+    new_color = (color[0] - increment, 0., color[2] + increment)
+    if node.left_node is not None:
+        draw_tree(node.left_node, not is_vertical_split, new_color)
+    if node.right_node is not None:
+        draw_tree(node.right_node, not is_vertical_split, new_color)
 
 
 def draw():
@@ -47,8 +55,8 @@ def draw():
 def draw_step(located_points, rectangle):
     draw()
     ax.add_patch(patches.Rectangle((rectangle[0][0], rectangle[0][1]),
-                rectangle[1][0] - rectangle[0][0], rectangle[1][1] - rectangle[0][1],
-                linewidth=1, edgecolor='g', facecolor='none'))
+                                   rectangle[1][0] - rectangle[0][0], rectangle[1][1] - rectangle[0][1],
+                                   linewidth=1, edgecolor='g', facecolor='none'))
     plt.plot(list(map(lambda x: x[0], located_points)), list(map(lambda x: x[1], located_points)), 'r.')
     camera.snap()
 
@@ -56,7 +64,7 @@ def draw_step(located_points, rectangle):
 def generate_points():
     generated_points = []
     for i in range(n):
-        generated_points.append([random.uniform(MINX, MAXX), random.uniform(MINY, MAXY)])
+        generated_points.append([(random.uniform(MINX, MAXX)), (random.uniform(MINY, MAXY))])
     return generated_points
 
 
@@ -70,24 +78,51 @@ def generate_rectangle():
     return rect
 
 
-def build_tree(current_points, is_vertical_split=True):
+def build_tree(current_points, is_vertical_split=True, minx=MINX - 1, maxx=MAXX + 1, miny=MINY - 1, maxy=MAXY + 1):
     if len(current_points) == 0:
         return None
-
+    # TODO copypaste
     if is_vertical_split:
         current_points.sort(key=lambda p: p[0])
+        median = len(current_points) // 2
+        midx = current_points[median][0]
+        return Node(current_points[median], current_points,
+                    minx, maxx, miny, maxy,
+                    build_tree(current_points[:median], not is_vertical_split, minx, midx, miny, maxy),
+                    build_tree(current_points[median + 1:], not is_vertical_split, midx, maxx, miny, maxy))
     else:
         current_points.sort(key=lambda p: p[1])
-    median = len(current_points)//2
+        median = len(current_points) // 2
+        midy = current_points[median][1]
+        return Node(current_points[median], current_points,
+                    minx, maxx, miny, maxy,
+                    build_tree(current_points[:median], not is_vertical_split, minx, maxx, miny, midy),
+                    build_tree(current_points[median + 1:], not is_vertical_split, minx, maxx, midy, maxy))
 
-    return Node(current_points[median], current_points,
-                build_tree(current_points[:median], not is_vertical_split),
-                build_tree(current_points[median + 1:], not is_vertical_split))
+
+def rec_search(rect, node):
+    result = []
+
+    if rect[0][0] <= node.point[0] <= rect[1][0] and \
+            rect[0][1] <= node.point[1] <= rect[1][1]:
+        result += [node.point]
+    if rect[0][0] > rect[1][0] or rect[0][1] > rect[1][1]:
+        return []
+    if rect[0][0] == node.minx and rect[0][1] == node.miny and \
+            rect[1][0] == node.maxx and rect[1][1] == node.maxy:
+        return node.current_points
+
+    if node.left_node is not None:
+        new_rect = [rect[0], (min(rect[1][0], node.left_node.maxx), min(rect[1][1], node.left_node.maxy))]
+        result += rec_search(new_rect, node.left_node)
+    if node.right_node is not None:
+        new_rect = [(max(rect[0][0], node.left_node.minx), max(rect[0][1], node.left_node.miny)), rect[1]]
+        result += rec_search(new_rect, node.right_node)
+    return result
 
 
 def regional_search(rect):
-    # TODO search
-    result = []
+    result = rec_search(rect, root)
     draw_step(result, rect)
 
 
